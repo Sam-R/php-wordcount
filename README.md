1c8bc5b5842e17a57753ee80dd8cabfe1a3a0a2e
sir8472 <sir8472@googlemail.com>
2015-07-29 13:30:04 +0000

    Added index.php containing a 'demo' of the wordcount function usage, and created 'getWordFrequency.php' to allow users to include an individual file in their own applications in order to access the functionality easily

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # php-wordcount
 PHP script that counts words in a file and returns an array
+
+## How to use
+
+The demo can be found in index.php, along with an example of calling the function and returning output to the screen.
+
+
+### Include in existing project
+
+The function itself is located in getWordFrequency.php and can be included quite easily into any other PHP project:
+
+
+```
+include('getWordFrequency.php');
+
+$arr-word-frequency = getWordFrequency("myfile.txt");
+
+```
+
+### Function Params
+
+The function has three paramiters:
+
+Paramiter  | Required | Effect
+------------- | ------------- | -------------
+file  | Yes | The file to open and run the function on.
+caseSensitive  | No | Defaults to false, defines whether the text should be scanned as case sensitive so words with upper case characters are counted seporately.
+alphaNumericOnly | No | Defaults to true, defines whether the text should be scanned only for alpha-numeric characters. If false, any punctuation and formatting will be counted as well.
---
+++ getWordFrequency.php
@@ -0,0 +1,41 @@
+<?php
+
+/** Gets the frequency of a word occuring in a text file
+ * @var file string the file name and path to be used
+ * @var caseSensitive bool should the count be case sensitive
+ * @var alphaNumericOnly bool should the count allow only alphanumeric chars
+ * @return array (word=>no. occurances)
+ */
+function GetWordFrequency($file, $caseSensitive=false, $alphaNumericOnly = true)
+{
+	// Get the file contents and store it as a string
+	$strFileContents = file_get_contents($file, 'r');
+	
+	// Check to see if we're using a case sensitive check
+	if(!$caseSensitive)
+	{
+		// if case insensitve, convert the string to lowercase
+		$strFileContents = strtolower($strFileContents);
+	}
+	
+	if($alphaNumericOnly)
+	{
+		//Allow alphanumberic and spaces only.
+		$strFileContents = ereg_replace("[^A-Za-z0-9 ]", "", $strFileContents );
+	}
+
+  
+	// Count the number of words in the file and convert to an array
+	// Note: use the word as the KEY so we can ksort later
+	$arrWords = str_word_count($strFileContents, 1);
+	
+	
+	// Get the frequency of each word in the array
+	$arrFrequency = array_count_values($arrWords);
+	
+	// Now use the ever useful ksort to organise by key value
+	ksort($arrFrequency);
+	
+	// Return sorted data as an array so it can be formatted later...
+	return $arrFrequency;
+}
